94055363f4f1335d80ac73ccad694acb27bca618
vti <viacheslav.t@gmail.com>
2014-09-25 22:40:31 +0300

    initial

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+App::Noti - Simple filesystem notification watcher
+
+# SYNOPSIS
+
+    use App::Noti;
+
+# DESCRIPTION
+
+See `noti` command instead.
+
+# LICENSE
+
+Copyright (C) Viacheslav Tykhanovskyi.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Viacheslav Tykhanovskyi <viacheslav.t@gmail.com>
---
+++ lib/App/Noti.pm
@@ -0,0 +1,39 @@
+package App::Noti;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+App::Noti - Simple filesystem notification watcher
+
+=head1 SYNOPSIS
+
+    use App::Noti;
+
+=head1 DESCRIPTION
+
+See C<noti> command instead.
+
+=head1 LICENSE
+
+Copyright (C) Viacheslav Tykhanovskyi.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Viacheslav Tykhanovskyi E<lt>viacheslav.t@gmail.comE<gt>
+
+=cut
+
